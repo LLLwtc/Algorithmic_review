1f98ab9b46539315201c84c1bb08b0b864b13276
wangyueying@pconline.com.cn <wangyueying@pconline.com.cn>
2023-07-20 16:11:42 +0800

    1. 使用set辅助完成数组作业

--- src/main/java/charpter3_array/homework.java
+++ src/main/java/charpter3_array/homework.java
@@ -1,28 +1,42 @@
 package charpter3_array;
 
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
 public class homework {
     public static void main(String[] args) {
-        int[] arr={1,1,2,2,3,4,5};
+        int[] arr = {1, 1, 2, 2, 3, 4, 5};
 //        int[] arr={0,2,2,2,2,2,5};
-        System.out.println(solve(arr));
+        int count = solve(arr);
+
+        for (int i = 0; i < count; i++) {
+            System.out.println(arr[i]);
+        }
+
+
     }
 
     public static int solve(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+
         int len = arr.length;
-        int ans = 1;
-
         int slow = 0;
-        int fast = 0;
-
-        while (fast < len && slow <= fast) {
-            if (arr[slow] == arr[fast]) fast++;
-            else {
-                if(fast-slow==1)ans++;
-                slow=fast;
+        for (int fast = 1; fast < len; fast++, slow++) {
+            if (arr[fast] == arr[slow]) {
+                set.add(arr[fast]);
             }
         }
-        return ans;
 
+        slow = 0;
+        for (int fast = 0; fast < len; fast++) {
+            if (!set.contains(arr[fast])) {
+                arr[slow] = arr[fast];
+                slow++;
+            }
+        }
+        return slow;
     }
 }
